61e6cb251113c22e272755666395f5c8db195e12
Eknath <you@example.com>
2021-08-05 10:00:15 +0530

    counter app

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import "../css/app.css";
+
+const App = () => {
+	const maxValue = 1000;
+	const minValue = 1;
+	const [count, setCount] = useState(minValue);
+
+	const increment = () => {
+		if (count < maxValue) {
+			setCount((parseInt(count) || 0) + 1);
+		}
+	}
+
+	const decrement = () => {
+		if (count > minValue) {
+			setCount((parseInt(count) || 1) - 1);
+		}
+	}
+
+	const isValidInput = (countVal) => {
+		return countVal === "" || (!isNaN(countVal) && countVal <= maxValue && countVal >= minValue);
+	}
+
+	const onInputValueChange = (event) => {
+		const inputVal = event.target.value;
+		if (isValidInput(inputVal)) {
+			setCount(parseInt(inputVal) || "");
+		}
+	}
+
+	return(
+		<div className="outer-wrapper">
+			<button
+				className="button-spec descrement-btn text-font"
+				onClick={decrement}
+			>
+				-
+			</button>
+			<input
+				type="text"
+				className="input-text text-font"
+				onChange={onInputValueChange}
+				value={count}
+			/>
+			<button
+				className="button-spec increment-btn text-font"
+				onClick={increment}
+			>
+				+
+			</button>
+		</div>
+	);
+};
+
+export default App;
